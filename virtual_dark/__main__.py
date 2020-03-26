b2e79753b3e8c1f97cbbaf67f61cf3fbd5e5d9ab
Jonas Nikula <jonas.nikula@gmail.com>
2020-03-26 19:21:05 +0100

    Add first argument
    Input images

--- virtual_dark/__main__.py
+++ virtual_dark/__main__.py
@@ -2,4 +2,9 @@ import argparse
 
 parser = argparse.ArgumentParser(description="Process negatives")
 
-parser.add_argument("input_imgs", metavar="img", type=)
+parser.add_argument("input_imgs", metavar="img", type=str, nargs="+", help="Input images")
+
+args = parser.parse_args()
+input_images = args.input_imgs
+
+print("Input images:\n\t" + "\n\t".join(map(str, input_images)))
